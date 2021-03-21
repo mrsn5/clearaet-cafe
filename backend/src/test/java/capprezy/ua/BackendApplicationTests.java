fdfaa11ac423d6eb671227cb52aab350b850baba
San Nguyen <mr.sn5.kma@gmail.com>
2021-03-21 14:19:53 +0200

    pagination

---
+++ backend/src/test/java/capprezy/ua/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package capprezy.ua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BackendApplicationTests {
+	@Test
+	public void contextLoads() throws Exception {
+	}
+
+}
